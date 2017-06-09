1f9343a07b07abaefed0fd7a359552974d280083
Vinlor <13824380425@163.com>
2017-06-09 11:02:46 +0800

    new WLMenuItem

---
+++ WLPageController/WLPageController/Views/WLMenuItem.h
@@ -0,0 +1,13 @@
+//
+//  WLMenuItem.h
+//  WLPageController
+//
+//  Created by dinpay on 2017/6/9.
+//  Copyright © 2017年 com.vinlor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLMenuItem : UILabel
+
+@end
---
+++ WLPageController/WLPageController/Views/WLMenuItem.m
@@ -0,0 +1,21 @@
+//
+//  WLMenuItem.m
+//  WLPageController
+//
+//  Created by dinpay on 2017/6/9.
+//  Copyright © 2017年 com.vinlor. All rights reserved.
+//
+
+#import "WLMenuItem.h"
+
+@implementation WLMenuItem
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
