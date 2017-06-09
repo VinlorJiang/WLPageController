005716eb44486b2dea9292a6cb5bc837f174fbfd
Vinlor <13824380425@163.com>
2017-06-09 10:49:35 +0800

    new project

---
+++ WLPageController/WLPageController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WLPageController
+//
+//  Created by dinpay on 2017/6/9.
+//  Copyright © 2017年 com.vinlor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WLPageController/WLPageController/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WLPageController
+//
+//  Created by dinpay on 2017/6/9.
+//  Copyright © 2017年 com.vinlor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
